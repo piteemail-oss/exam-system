284a2a13c917533170e59fe025b4f2ab02ae229e
xiaojiu <310485363@qq.com>
2026-06-05 07:52:51 +0800

    降低答对音效音量

--- frontend/src/utils/sound.js
+++ frontend/src/utils/sound.js
@@ -20,7 +20,7 @@ export function playCorrectSound() {
       osc.type = 'triangle'
       osc.frequency.value = freq
       gain.gain.setValueAtTime(0, now + i * 0.08)
-      gain.gain.linearRampToValueAtTime(0.25, now + i * 0.08 + 0.02)
+      gain.gain.linearRampToValueAtTime(0.15, now + i * 0.08 + 0.02)
       gain.gain.exponentialRampToValueAtTime(0.001, now + i * 0.08 + 0.2)
       osc.connect(gain)
       gain.connect(ctx.destination)
